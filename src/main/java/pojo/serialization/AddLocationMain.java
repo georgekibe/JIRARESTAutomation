2ac16ac5c28a2d42102e06c49fd8020acaad0608
georgekibe <georgekibe500@gmail.com>
2024-10-14 22:07:05 +0300

    Added a location using POJO serialization

--- src/main/java/pojo/serialization/AddLocationMain.java
+++ src/main/java/pojo/serialization/AddLocationMain.java
@@ -3,6 +3,9 @@ package pojo.serialization;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 /*
@@ -17,10 +20,33 @@ public class AddLocationMain {
     @Test
     public void addingLocation() {
 
-       String addLocationResponse =  given().queryParam("key", "qaclick123")
-                .body()
+        // initialize the payload parameters using the pojo serialization
+        LocationDetails locationDetails = new LocationDetails();
+
+        Location location =  new Location();
+        location.setLat(-31.48488);
+        location.setLng(75.51515);
+        locationDetails.setLocation(location);
+
+        locationDetails.setAccuracy(50);
+        locationDetails.setName("Nairobi Kenya");
+        locationDetails.setPhone_number("+254728852318");
+        locationDetails.setAddress("Parklands, Nairobi");
+        locationDetails.setWebsite("https://google.com");
+        locationDetails.setLanguage("Kiswahili");
+
+        List types = new ArrayList();
+        types.add("shoe park");
+        types.add("shop");
+        locationDetails.setTypes(types);
+
+
+        // request payload
+        String addLocationResponse = given().queryParam("key", "qaclick123")
+                .body(locationDetails)
                 .when().post("https://rahulshettyacademy.com/maps/api/place/add/json")
                 .then().assertThat().statusCode(200).extract().asString();
+        System.out.println("The Response: " + addLocationResponse);
 
     }
 
