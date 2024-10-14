6d2bc68cc67925fa03155cb9a762e4639a4dee86
georgekibe <georgekibe500@gmail.com>
2024-10-14 21:01:51 +0300

    Done with serialization

--- src/main/java/pojo/GetCourseDetails.java
+++ src/main/java/pojo/GetCourseDetails.java
@@ -2,7 +2,12 @@ package pojo;
 
 
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -26,43 +31,57 @@ public class GetCourseDetails {
                 .when().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token")
                 .then().assertThat().statusCode(200).extract().asString();
 
-        JsonPath js  =  new JsonPath(tokenResponse);
-         access_token = js.getString("access_token");
+        JsonPath js = new JsonPath(tokenResponse);
+        access_token = js.getString("access_token");
     }
 
     @Test
-    public void getCourseDetails(){
+    public void getCourseDetails() {
         // get course details
 
-       GetPojoCourseDetails getCourseDetailsResponse =  given().pathParam("access_token", access_token)
+        GetPojoCourseDetails getCourseDetailsResponse = given().pathParam("access_token", access_token)
                 .when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails?access_token={access_token}")
                 .then().log().all().extract().as(GetPojoCourseDetails.class);
 
 
-      // print the linkedIn URL
-       String linkedIn =  getCourseDetailsResponse.getLinkedIn();
-      System.out.println(linkedIn);
-      // print the Cypress Course Price
-      String cypressPrice = getCourseDetailsResponse.getCourses().getWebAutomation().get(0).getPrice();
-      System.out.println(cypressPrice);
-      // print the mobile entry that is for Appium
-      String mobileCourseTitle = getCourseDetailsResponse.getCourses().getMobile().get(0).getCourseTitle();
-      System.out.println(mobileCourseTitle);
+        // print the linkedIn URL
+        String linkedIn = getCourseDetailsResponse.getLinkedIn();
+        System.out.println(linkedIn);
+        // print the Cypress Course Price
+        String cypressPrice = getCourseDetailsResponse.getCourses().getWebAutomation().get(0).getPrice();
+        System.out.println(cypressPrice);
+        // print the mobile entry that is for Appium
+        String mobileCourseTitle = getCourseDetailsResponse.getCourses().getMobile().get(0).getCourseTitle();
+        System.out.println(mobileCourseTitle);
 
-      /// printing the sum for all the course under Web Automation
+        /// printing the sum for all the course under Web Automation
 
-    int arraySize = getCourseDetailsResponse.getCourses().getWebAutomation().size();
-    System.out.println("Size: " +arraySize);
+        int arraySize = getCourseDetailsResponse.getCourses().getWebAutomation().size();
+        System.out.println("Size: " + arraySize);
 
-    int sum=0;
-    for (int i=0 ; i < arraySize; i++)
-    {
-        int price  = Integer.parseInt(getCourseDetailsResponse.getCourses().getWebAutomation().get(i).getPrice());
-        sum += price;
+        int sum = 0;
+        for (int i = 0; i < arraySize; i++) {
+            int price = Integer.parseInt(getCourseDetailsResponse.getCourses().getWebAutomation().get(i).getPrice());
+            sum += price;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        // initialize course title and compare with output using assertion to match
+        String[] expectedCourseTitles =  {"Selenium Webdriver Java", "Cypress", "Protractor"};
+        List <String> expectedList = Arrays.asList(expectedCourseTitles); // convert string array to array list
+
+        ArrayList<String> actualList = new ArrayList<String>();
+        List<WebAutomation> titles = getCourseDetailsResponse.getCourses().getWebAutomation();
+
+        for(int k=0; k<titles.size(); k++)
+        {
+            actualList.add(titles.get(k).getCourseTitle());
+        }
+
+        Assert.assertEquals(expectedList, actualList);
+
     }
 
-    System.out.println("Sum: " + sum);
-
-    }
 
 }
